f6ae30c159612e827f52b6790adb793d8a05e0d6
“Regina” <reginajuniantika124@gmail.com>
2024-06-23 23:17:04 +0700

    push

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/apple-icon.png') }} ">
     <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png') }} ">
-    <title>Material Dashboard 2 by Creative Tim</title>
+    <title>UJIKOM WEB DEVELOP</title>
     <!-- Fonts and icons -->
     <link rel="stylesheet" type="text/css"
         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
